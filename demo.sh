09c5e76efbe53143a3929f46b5f8f8d5453311d8
zhaochanggang <donaldjohn@qq.com>
2019-07-29 19:44:17 +0800

    routine

--- demo.sh
+++ demo.sh
@@ -17,3 +17,54 @@ curl "$URI?pretty" -d '
         }
     }
 }'
+
+curl "$URI?pretty" -d '
+{
+    "aggregations":
+    {
+        "tags":
+        { 
+            "terms":
+            { 
+                "field": "tags.verbatim"
+            }
+        }
+    }
+}'
+
+
+curl "$URI?pretty" -d '
+{
+    "aggregations":
+    {
+        "tags":
+        { 
+            "terms":
+            { 
+                "field": "tags.verbatim",
+                "include":".*search.*"
+            }
+        }
+    }
+}'
+
+
+curl "$URI?pretty" -d '
+{
+    "aggregations":
+    {
+        "attendees_breakdown":
+        { 
+                "range":
+                {
+                    "script":"doc['"'attendees'"'].values.length",
+                    "ranges":
+                    [
+                        {"to":4},
+                        {"from":4, "to":6},
+                        {"from":6}
+                    ]
+                }
+        }
+    }
+}'
